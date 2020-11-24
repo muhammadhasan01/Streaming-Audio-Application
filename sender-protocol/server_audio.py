c68969092c20cadcaef05fea7605a8a247b145b2
donbasta <faddilafarras@gmail.com>
2020-11-24 11:16:09 +0700

    restructuring

---
+++ sender-protocol/server_audio.py
@@ -0,0 +1,182 @@
+import os
+import socket
+import time
+import threading
+import math
+import packet
+import const
+from collections import deque
+from wavhandler import WavHandler
+
+class SenderThread(threading.Thread):
+
+	def __init__(self, sock, server_address, fname, sequence, final=False, meta=False):
+		super(SenderThread, self).__init__()
+		self.sock = sock
+		self.server_address = server_address
+		self.fname = fname
+		self.sequence = sequence
+		self.final = final
+		self.meta = meta
+		self.stopped = False
+
+	def get_data(self):
+		if self.meta:
+			data = fname.encode('utf-8')
+		else:
+			offset = self.sequence * const.MAX_LENGTH
+			f = open(self.fname, 'rb')
+			f.seek(offset)
+			data = f.read(const.MAX_LENGTH)
+			f.close()
+
+		return data
+
+	def send_packet(self):
+		data = self.get_data()
+		if self.final:
+			p = packet.Packet(const.TYPE_FIN, self.sequence, data)
+		elif self.meta:
+			p = packet.Packet(const.TYPE_MDATA, self.sequence, data)
+		else:
+			p = packet.Packet(const.TYPE_DATA, self.sequence, data)
+
+		self.sock.sendto(p.to_bytes(), self.server_address)
+
+	def stop(self):
+		self.stopped = True
+
+	def run(self):
+		while not self.stopped:
+			self.send_packet()
+			print("Packet #{} is being sent to {}.".format(self.sequence, self.server_address))
+
+			t = 0
+			while t < const.RESEND_TIMER:
+				t += const.RESEND_TIMER_TICK
+				time.sleep(const.RESEND_TIMER_TICK)
+				if self.stopped:
+					print("Packet #{} not restarting...".format(self.sequence))
+					break
+# SENDER
+
+class StreamThread(threading.Thread):
+
+	def __init__(self, fpath, subscribers=[], chunk_size):
+		self.fpath = fpath;
+		self.subscribers = subscribers
+		self.chunk_size = chunk_size
+		self.wav = WavHandler(fpath)
+
+		#TODO: pecah2 chunk di fpath jadi chunks, without metadata
+		self.chunks = self.wav.chunks
+	
+	def add_subscriber(self, subscriber):
+    	self.subscribers.append(subscriber)
+	
+	def run(self):
+		# TODO: get from metadata
+		nChannels = self.wav.metadata["num_channels"] # Audio channel count, just for example, get from the WAV metadata.
+		sampleWidth = self.wav.metadta["sample_width"]  # Audio sample width, just for example, get from the WAV metadata.
+		frameRate = self.wav.metadata["sample_rate"] # Audio frame rate, just for example, get from the WAV metadata.
+
+		frameSize = nChannels * sampleWidth # In bytes
+		frameCountPerChunk = CHUNK_SIZE / frameSize
+
+		chunkTime = 1000 * frameCountPerChunk / frameRate # In milliseconds.
+
+		for chunk in self.chunks:
+			startTime = time.time()
+			for subscriber in subscribers:
+				send(chunk, subscriber)
+			endTime = time.time()
+			delta = endTime - startTime
+			if delta < chunkTime:
+    			time.sleep(chunkTime - delta) # Sleep for the remaining time if there is any.
+			# Continue to next chunk
+
+def send(chunk, subscriber):
+	"""
+	chunk: data
+	subscriber: IP address subscriber
+	"""
+	# TODO: bikin send buat StreamThread
+	pass 
+
+
+def add_subscriber(stream_thread, subscriber):
+    """
+	stream_thread: thread for streaming
+	subscriber: IP address for new subscriber
+	"""
+    stream_thread.add_subscriber(subscriber)
+
+
+
+class ListenerThread(threading.Thread):
+
+	def __init__(self, fpath, stream_thread):
+		self.fpath = fpath
+		self.wav = WavHandler(fpath)
+		self.metadata = self.wav.metadata
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.stream_thread = stream_thread
+
+		self.total_threads_pool = deque()
+
+		if const.FILE_METADATA:
+			self.total_threads_pool.append(
+				SenderThread(self.sock, self.server_address, self.fname, 0, meta=True)	
+			)
+
+		for i in range(self.total_chunks-1):
+			self.total_threads_pool.append(
+				SenderThread(self.sock, self.server_address, self.fname, i, final=False)	
+			)
+
+		self.final = SenderThread(self.sock, self.server_address, self.fname, total_chunks-1, final=True)
+
+	def run(self):
+		while True:
+			p, address = self.sock.recvfrom(const.MAX_LENGTH)
+			# TODO: Ngirim paket metadata
+			
+			# TODO: Add subscriber
+			add_subscriber(self.stream_thread, address)
+
+
+def recv_packet(sock):
+	data, addr = sock.recvfrom(const.MAX_PACKET_LENGTH)
+	p = packet.Packet.to_packet(data)
+	return p, addr
+
+def get_file_size(path):
+	return os.stat(path).st_size
+
+def get_file_chunks(path):
+	return math.ceil(get_file_size(path)/const.MAX_LENGTH)
+
+def to_addresses(addresses, port):
+	server_addresses = []
+	for address in addresses:
+		server_addresses.append((address, port))
+	return server_addresses
+
+if __name__ == "__main__":
+
+	port = sys.argv[1]
+	fpath = sys.argv[2]
+			
+	wav = WavHandler(fpath)
+	metadata = wav.metadata
+	chunk_size = wav.chunks
+	RECV_PORT = int(input("Input port of receivers: "))
+
+	stream_thread = StreamThread(fpath=fpath, subscribers=[], chunk_size=chunk_size)
+	
+	print("Listening to subscribe request...")
+	listener_thread = ListenerThread(fpath=fpath, stream_thread=stream_thread)
+	listener_thread.start()
+
+	print("Start sending audio packets...")
+	stream_thread.start()
--- wavhandler.py
+++ wavhandler.py
@@ -2,49 +2,56 @@ import wave
 import sys
 import pyaudio  
 
+class WavHandler():
+    def __init__(self, fpath):
+        self.file = wave.open(fpath,"rb")
+        self.metadata = self.get_metadata_audio()
+        self.chucks = self.get_chunks_audio()
 
-def play_wav_audio(filepath):
-    #define stream chunk   
-    chunk = 1024
+    def play_wav_audio(self):
+        #define stream chunk   
+        chunk = 1024
 
-    #open a wav format music  
-    f = wave.open(filepath,"rb")  
-    #instantiate PyAudio  
-    player = pyaudio.PyAudio()  
-    #open stream  
-    stream = player.open(format = player.get_format_from_width(f.getsampwidth()),  
-                    channels = f.getnchannels(),  
-                    rate = f.getframerate(),  
-                    output = True)  
-    #read data  
-    data = f.readframes(chunk)  
-
-    #play stream  
-    while data:  
-        stream.write(data)  
+        #open a wav format music  
+        f = self.file
+        #instantiate PyAudio  
+        player = pyaudio.PyAudio()  
+        #open stream  
+        stream = player.open(format = player.get_format_from_width(f.getsampwidth()),  
+                        channels = f.getnchannels(),  
+                        rate = f.getframerate(),  
+                        output = True)  
+        #read data  
         data = f.readframes(chunk)  
 
-    #stop stream  
-    stream.stop_stream()  
-    stream.close()  
+        #play stream  
+        while data:  
+            stream.write(data)  
+            data = f.readframes(chunk)  
 
-    #close PyAudio  
-    player.terminate()  
+        #stop stream  
+        stream.stop_stream()  
+        stream.close()  
 
-def get_metadata_audio(filepath):
+        #close PyAudio  
+        player.terminate()  
 
-    file = wave.open(filepath, "r")
+    def get_metadata_audio(self):
+        file = self.file
 
-    #sample width
-    sample_width = file.getsampwidth()
-    #num channel
-    num_channels = file.getnchannels()
-    #sample rate
-    sample_rate = file.getframerate()
+        #sample width
+        sample_width = file.getsampwidth()
+        #num channel
+        num_channels = file.getnchannels()
+        #sample rate
+        sample_rate = file.getframerate()
 
-    return {"sample_width": sample_width, 
-            "num_channels": num_channels, 
-            "sample_rate": sample_rate}
+        return {"sample_width": sample_width, 
+                "num_channels": num_channels, 
+                "sample_rate": sample_rate}
+
+    def get_chunks_audio(self):
+        return []
 
 if __name__ == "__main__":
     port = sys.argv[1]
@@ -53,7 +60,9 @@ if __name__ == "__main__":
     print("port: {}".format(port))
     print("wav_file_path: {}".format(wav_file_path))
 
-    metadata = get_metadata_audio(wav_file_path)
+    wav = WavHandler(wav_file_path)
+
+    metadata = wav.get_metadata_audio()
     print("metadata: {}".format(metadata))
 
-    play_wav_audio(wav_file_path)
+    wav.play_wav_audio()
