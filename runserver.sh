eaefd816c7ffd5c941abfc34cc951efc67ac2301
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-11-25 17:08:04 +0700

    Change runserver script

--- runserver.sh
+++ runserver.sh
@@ -5,4 +5,4 @@
 # $2 is the path file to wav audio (example: ./sample_wav/test1.wav)
 # Running Exampe: runserver.sh 5555 ./sample_wav/hallking.wav
 
-python server_audio.py "$1" "$2" 5555 ./sample_wav/hallking.wav
+python server_audio.py "$1" "$2"
