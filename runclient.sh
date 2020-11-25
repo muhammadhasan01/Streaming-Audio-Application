aa33f9e49929269b353d9b4bcfe5677e548e5c9f
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-11-25 16:10:50 +0700

    Create script `sh` files

---
+++ runclient.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script to run client
+# $1 is the IPAdress of the server (example: 192.168.1.46)
+# Running Exampe: runclient.sh 192.168.1.46
+
+python client_audio.py 192.168.1.46
---
+++ runserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script to run server
+# $1 is the port to bind the server with (example: 5555)
+# $2 is the path file to wav audio (example: ./sample_wav/test1.wav)
+# Running Exampe: runserver.sh 5555 ./sample_wav/test1.wav
+
+python server_audio.py "$1" "$2" 5555 ./sample_wav/test1.wav
